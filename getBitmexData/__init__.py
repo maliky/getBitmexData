0d326a98fce6b07151e59d946c3cca38efb2803c
mk@idev <malik.kone.etu@univ-lemans.fr>
2020-04-07 11:31:56 +0000

    adding init to .gitignore

---
+++ getBitmexData/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# put here documentation, pacakges constants
+
+# Version of the getBitmexData
+__version__ = "1.0.0"
+__description__ = "Download Bitmex historical data with different time resolution."
