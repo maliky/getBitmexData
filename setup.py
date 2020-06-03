fc868343f3bc923487e911aeaf1cd13f44f757a1
mk@idev <malik.kone.etu@univ-lemans.fr>
2020-06-03 19:41:40 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name="getBitMEXData",
-    version="0.0.4",
+    version="0.0.5",
     description="Utility to download bitMEX historical price data",
     long_description=long_description,
     long_description_content_type="text/x-rst",
